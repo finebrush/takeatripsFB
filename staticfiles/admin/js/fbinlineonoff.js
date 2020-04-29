5e833570bd7ec9cc3586aacdc8af4653c0a56100
finebrush.mlab@gmail.com <finebrush.mlab@gmail.com>
2020-04-29 13:52:01 +0900

    2rd complete

--- staticfiles/admin/js/fbinlineonoff.js
+++ staticfiles/admin/js/fbinlineonoff.js
@@ -22,125 +22,142 @@ $(document).ready( function() {
     //     }
     // }).attr('selected', true);
 
-    var sel = document.getElementById("id_part");
+    if (document.getElementById("id_part")){
 
-    // inline 그룹을 hide..
-    switch (sel.value) {
-        case "Eat":
-            $("#eatdrinkpart-group").show();
-            $("#funpart-group").hide();
-            $("#seepart-group").hide();
-            $("#sleeppart-group").hide();
-            $("#buypart-group").hide();
-            break;
-        case "Drink":
-            $("#eatdrinkpart-group").show();
-            $("#funpart-group").hide();
-            $("#seepart-group").hide();
-            $("#sleeppart-group").hide();
-            $("#buypart-group").hide();
-            break;
-        case "Fun":
-            $("#eatdrinkpart-group").hide();
-            $("#funpart-group").show();
-            $("#seepart-group").hide();
-            $("#sleeppart-group").hide();
-            $("#buypart-group").hide();
-            break;
-        case "See":
-            $("#eatdrinkpart-group").hide();
-            $("#funpart-group").hide();
-            $("#seepart-group").show();
-            $("#sleeppart-group").hide();
-            $("#buypart-group").hide();
-            break;
-        case "Sleep":
-            $("#eatdrinkpart-group").hide();
-            $("#funpart-group").hide();
-            $("#seepart-group").hide();
-            $("#sleeppart-group").show();
-            $("#buypart-group").hide();
-            break;
-        case "Buy":
-            $("#eatdrinkpart-group").hide();
-            $("#funpart-group").hide();
-            $("#seepart-group").hide();
-            $("#sleeppart-group").hide();
-            $("#buypart-group").show();
-            break;
-        default:
-            $("#eatdrinkpart-group").hide();
-            $("#funpart-group").hide();
-            $("#seepart-group").hide();
-            $("#sleeppart-group").hide();
-            $("#buypart-group").hide();
-            break;
-    }
-    
-    // 콤보박스의 변화가 생기면 선택된 텍스트를 판별하여 이벤트 발생..
-    $( "#id_part" ).change(function() {
-        // console.log(sel.value);
+        var sel = document.getElementById("id_part");
+
+        // inline 그룹을 hide..
         switch (sel.value) {
             case "Eat":
-                $("#eatdrinkpart-group").show();
+                $("#eatpart-group").show();
+                $("#drinkpart-group").hide();
                 $("#funpart-group").hide();
                 $("#seepart-group").hide();
                 $("#sleeppart-group").hide();
                 $("#buypart-group").hide();
                 break;
             case "Drink":
-                $("#eatdrinkpart-group").show();
+                $("#eatpart-group").hide();
+                $("#drinkpart-group").show();
                 $("#funpart-group").hide();
                 $("#seepart-group").hide();
                 $("#sleeppart-group").hide();
                 $("#buypart-group").hide();
                 break;
             case "Fun":
-                $("#eatdrinkpart-group").hide();
+                $("#eatpart-group").hide();
+                $("#drinkpart-group").hide();
                 $("#funpart-group").show();
                 $("#seepart-group").hide();
                 $("#sleeppart-group").hide();
                 $("#buypart-group").hide();
                 break;
             case "See":
-                $("#eatdrinkpart-group").hide();
+                $("#eatpart-group").hide();
+                $("#drinkpart-group").hide();
                 $("#funpart-group").hide();
                 $("#seepart-group").show();
                 $("#sleeppart-group").hide();
                 $("#buypart-group").hide();
                 break;
             case "Sleep":
-                $("#eatdrinkpart-group").hide();
+                $("#eatpart-group").hide();
+                $("#drinkpart-group").hide();
                 $("#funpart-group").hide();
                 $("#seepart-group").hide();
                 $("#sleeppart-group").show();
                 $("#buypart-group").hide();
                 break;
             case "Buy":
-                $("#eatdrinkpart-group").hide();
+                $("#eatpart-group").hide();
+                $("#drinkpart-group").hide();
                 $("#funpart-group").hide();
                 $("#seepart-group").hide();
                 $("#sleeppart-group").hide();
                 $("#buypart-group").show();
                 break;
             default:
-                $("#eatdrinkpart-group").hide();
+                $("#eatpart-group").hide();
+                $("#drinkpart-group").hide();
                 $("#funpart-group").hide();
                 $("#seepart-group").hide();
                 $("#sleeppart-group").hide();
                 $("#buypart-group").hide();
                 break;
         }
-        // if ($("#id_part option:selected").text() == "먹다/마시다"){
-        //     $("#eatdrinkpart-group").show();
-        // }else {
-        //     $("#eatdrinkpart-group").hide();
-        // }
-        // alert($("#id_part option:selected").text());
-        // console.log($("#id_part option:selected").val());
-    });
+        
+        // 콤보박스의 변화가 생기면 선택된 텍스트를 판별하여 이벤트 발생..
+        $( "#id_part" ).change(function() {
+            // console.log(sel.value);
+            switch (sel.value) {
+                case "Eat":
+                    $("#eatpart-group").show();
+                    $("#drinkpart-group").hide();
+                    $("#funpart-group").hide();
+                    $("#seepart-group").hide();
+                    $("#sleeppart-group").hide();
+                    $("#buypart-group").hide();
+                    break;
+                case "Drink":
+                    $("#eatpart-group").hide();
+                    $("#drinkpart-group").show();
+                    $("#funpart-group").hide();
+                    $("#seepart-group").hide();
+                    $("#sleeppart-group").hide();
+                    $("#buypart-group").hide();
+                    break;
+                case "Fun":
+                    $("#eatpart-group").hide();
+                    $("#drinkpart-group").hide();
+                    $("#funpart-group").show();
+                    $("#seepart-group").hide();
+                    $("#sleeppart-group").hide();
+                    $("#buypart-group").hide();
+                    break;
+                case "See":
+                    $("#eatpart-group").hide();
+                    $("#drinkpart-group").hide();
+                    $("#funpart-group").hide();
+                    $("#seepart-group").show();
+                    $("#sleeppart-group").hide();
+                    $("#buypart-group").hide();
+                    break;
+                case "Sleep":
+                    $("#eatpart-group").hide();
+                    $("#drinkpart-group").hide();
+                    $("#funpart-group").hide();
+                    $("#seepart-group").hide();
+                    $("#sleeppart-group").show();
+                    $("#buypart-group").hide();
+                    break;
+                case "Buy":
+                    $("#eatpart-group").hide();
+                    $("#drinkpart-group").hide();
+                    $("#funpart-group").hide();
+                    $("#seepart-group").hide();
+                    $("#sleeppart-group").hide();
+                    $("#buypart-group").show();
+                    break;
+                default:
+                    $("#eatpart-group").hide();
+                    $("#drinkpart-group").hide();
+                    $("#funpart-group").hide();
+                    $("#seepart-group").hide();
+                    $("#sleeppart-group").hide();
+                    $("#buypart-group").hide();
+                    break;
+            }
+            // if ($("#id_part option:selected").text() == "먹다/마시다"){
+            //     $("#eatdrinkpart-group").show();
+            // }else {
+            //     $("#eatdrinkpart-group").hide();
+            // }
+            // alert($("#id_part option:selected").text());
+            // console.log($("#id_part option:selected").val());
+        });
 
-    // $("input").click( function(event) {
-    //     alert("You clicked the button using JQuery!");
-    // });
+        // $("input").click( function(event) {
+        //     alert("You clicked the button using JQuery!");
+        // });
+    }
 });
